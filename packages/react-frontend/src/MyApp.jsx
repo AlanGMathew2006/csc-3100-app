aa5569fba77d4b83ed5aeb60899e698d59db64ff
Alan Mathew <alangmathew@gmail.com>
2026-09-15 13:36:35 -0700

    commit -m "Refactor MyApp.jsx to simplify fetch and post functions"

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,25 +1,53 @@
 // src/MyApp.jsx
 import Table from "./Table";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
+
+  function fetchUsers() {
+    return fetch("http://localhost:8000/users");
+  }
+
+  function postUser(person) {
+    return fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
     });
+  }
+
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => i !== index);
     setCharacters(updated);
   }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList} />
     </div>
   );
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
 }
 
 export default MyApp;
